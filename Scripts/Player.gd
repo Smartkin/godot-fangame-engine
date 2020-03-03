d73a8de66ea408ab730254496676a959df1932e2
Smartkin <vladislav5019@gmail.com>
2020-03-03 03:58:06 +0300

    Initial commit

---
+++ Scripts/Player.gd
@@ -0,0 +1,80 @@
+extends KinematicBody2D
+
+const UP = Vector2(0, -1)
+const SLOPE_SNAP = Vector2(0, -10)
+const DEBUG_OUTPUT_RATE = 60
+
+var speed = Vector2(0, 0)
+var canJump = true
+var canDjump = true
+var runSpeed = 200
+var jumpHeight = -450
+var djumpHeight = -350
+var gravity = 20
+var debugOutputTimer = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$Sprite.play("Idle")
+
+func _physics_process(_delta):
+	debugOutputTimer += 1
+	handleInputs()
+	applyGravity()
+	if (is_on_floor()):
+		canJump = true
+		canDjump = true
+	else:
+		canJump = false
+	speed = move_and_slide_with_snap(speed, SLOPE_SNAP, UP)
+	if (speed.x == 0 && speed.y == 0):
+		$Sprite.play("Idle")
+	if (speed.y > 0):
+		$Sprite.play("Fall")
+	if (debugOutputTimer >= DEBUG_OUTPUT_RATE):
+		debugOutputTimer = 0
+		print_debug("Is on floor: " + String(is_on_floor()))
+		print_debug("Is on ceiling: " + String(is_on_ceiling()))
+		print_debug("Is touching wall: " + String(is_on_wall()))
+
+# Player jumping logic
+func jump():
+	var jumped = false
+	if (canJump):
+		speed.y = jumpHeight
+		canJump = false
+		jumped = true
+	elif (canDjump):
+		speed.y = djumpHeight
+		canDjump = false
+		jumped = true
+	if (jumped):
+		$Sprite.play("Jump")
+
+func cutJump():
+	speed.y *= 0.5
+
+func applyGravity():
+	speed.y += gravity
+
+# Player running logic
+func run(direction):
+	speed.x = runSpeed * direction
+	if (direction != 0):
+		if (speed.y == 0):
+			$Sprite.play("Run")
+		$Sprite.flip_h = (direction == -1)
+
+func handleInputs():
+	# Handle player jump
+	if (Input.is_action_just_pressed("pl_jump")):
+		jump()
+	if (Input.is_action_just_released("pl_jump")):
+		cutJump()
+	# Handle player run
+	if (Input.is_action_pressed("pl_left")):
+		run(-1)
+	elif (Input.is_action_pressed("pl_right")):
+		run(1)
+	else:
+		run(0)
